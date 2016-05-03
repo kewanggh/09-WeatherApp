6fa6cad49ef4de9eab5e7b7b2e8899d5cb6ed18b
Ke Wang <Ke Wang>
2016-05-03 09:21:14 -0700

    first commit

---
+++ app/index.js
@@ -0,0 +1,11 @@
+(function(){
+
+	'use strict'
+	//define top-level module container
+	var app = angular.module('app',[]);
+	//additional configuration goes here
+
+
+})();
+
+// a self closing function, when the page loads, it will automatically loads.
